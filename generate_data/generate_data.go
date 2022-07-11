25e5939f8e3b43e8bc68a15a961fd29b28f3d758
evaachim <44099125+evaachim@users.noreply.github.com>
2022-07-11 09:51:29 -0400

    Update generate_data.go

--- generate_data/generate_data.go
+++ generate_data/generate_data.go
@@ -4,19 +4,13 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
+
 	"github.com/google/uuid"
 	"github.com/project-safari/zebra"
 )
 
-type Human struct {
-	name     string
-	password string
-}
-
-func (h Human) User() map[string]string {
-
+func User() map[string]string {
 	fmt.Println("Test usernames")
-
 	people := make(map[string]string)
 
 	// take care of usernames
@@ -24,15 +18,20 @@ func (h Human) User() map[string]string {
 		"user1",
 		"user2",
 		"user3",
+		"manager",
+		"admin",
+		"guest",
 		"another-user",
 		" ",
 	}
-	//usernames := []string{}
 
 	password_list := []string{
 		"pass1",
 		"pass2",
 		"pass3",
+		"manager",
+		"admin-pass",
+		" ",
 		"another-password",
 		" ",
 	}
@@ -40,31 +39,21 @@ func (h Human) User() map[string]string {
 	for i := 0; i < len(name_list); i++ {
 
 		if name_list[i] != " " {
-			h.name = name_list[i]
-			h.password = password_list[i]
+			name := name_list[i]
+			password := password_list[i]
 			//usernames = append(usernames, h.name)
-			people[h.name] = h.password
+			people[name] = password
 		}
 	}
-
-	/*
-		//passwords := []string{}
-		for i := 0; i < len(password_list); i++ {
-
-			if password_list[i] != " " {
-				h.password = password_list[i]
-				//passwords = append(passwords, h.password)
-			}
-		}
-	*/
+	
 	return people
 }
 
-func RandType(res[]string) string {
+func RandType(res []string) string {
 	rand.Seed(time.Now().UnixNano())
 	var length = len(res)
 	var ind int = rand.Intn(length - 1)
-	typ := resType[ind]
+	typ := res[ind]
 	return typ
 }
 
@@ -72,13 +61,13 @@ func CreateLabels() map[string]string {
 	codes := make(map[string]string)
 	col := " "
 	let := " "
-	colors := []string{"red", "yellow", "green", "blue", "white","magenta", "black", "purple", "brown", "orange", "pink", "grey"}
-	letters := []string{"alpha", "beta", "gamma", "delta", "epsilon", "eta", "theta", "Iota", "Kappa", "Lambda", "Mu", "Nu"}]
+	colors := []string{"red", "yellow", "green", "blue", "white", "magenta", "black", "purple", "brown", "orange", "pink", "grey"}
+	letters := []string{"alpha", "beta", "gamma", "delta", "epsilon", "eta", "theta", "Iota", "Kappa", "Lambda", "Mu", "Nu"}
 
-	for i:=0; i<len(colors); i++{
+	for i := 0; i < len(colors); i++ {
 		col = RandType(colors)
 		let = RandType(letters)
-		codes[let] = col 
+		codes[let] = col
 	}
 
 	return codes
@@ -101,4 +90,17 @@ func CreateBaseResource(resType string, labels zebra.Labels) *zebra.BaseResource
 
 var resourceTypes = []string{"VLANPool", "Switch", "IPAddressPool", "Datacenter", "Lab", "Rack", "Server", "ESX", "VM", " ", "BaseResource", "NamedResource", "Credentials"}
 
-//var Ids = []string{"01000000", "01000001", "01000002", "01000003", "01000004", "01000005", "01000006", "01000007", "02000000", "02000001", "02000002", "02000003", "02000004", "02000005", "02000006", "02000007", "03000000", "03000001", "03000002", "03000003", "03000004", "03000005", "03000006", "03000007", "10000000", "10000001", "10000003", "10000004", "10000005", "10000006", "10000007", "20000000", "20000001", "20000002", "20000003", "20000004", "20000005", "20000006", "20000007", "30000000", "30000001", "30000002", "30000003", "30000004", "30000005", "30000006", "30000007"}
+func Generate_Data() {
+
+	credentials := User()
+	all := make(map[interface{}]interface{})
+	//all := make(map[string]*zebra.BaseResource)
+	for each := 0; each < 100; each++ {
+
+		theType := RandType(resourceTypes)
+		theLabels := CreateLabels()
+		theData := CreateBaseResource(theType, theLabels)
+		fmt.Println("The username, password, corresponding resource and its respective labels: ", theData)
+		all[credentials] = theData
+	}
+}
