b95b7708c592bf117e7feec2c42e60771f296d23
Shravya Nandyala <shravya.nandyala@gmail.com>
2022-07-19 10:54:30 -0700

    added status validation, string, and marshal
    - modified Status fields to use iota
    - added Status validation in BaseResource validation to enforce for all resources
    
    Signed-off-by: Shravya Nandyala <shravya.nandyala@gmail.com>

--- status_test.go
+++ status_test.go
@@ -15,10 +15,10 @@ func TestDefaultStatus(t *testing.T) {
 
 	s := zebra.DefaultStatus()
 	assert.NotNil(s)
-	assert.Equal("none", s.Fault)
-	assert.Equal("free", s.Lease)
+	assert.Equal(zebra.None, s.Fault)
+	assert.Equal(zebra.Free, s.Lease)
 	assert.Equal("", s.UsedBy)
-	assert.Equal("active", s.State)
+	assert.Equal(zebra.Active, s.State)
 }
 
 func TestValidateStatus(t *testing.T) {
@@ -26,25 +26,57 @@ func TestValidateStatus(t *testing.T) {
 	assert := assert.New(t)
 
 	s := zebra.Status{
-		Fault:       "random1",
-		Lease:       "random2",
-		UsedBy:      "random3",
-		State:       "random4",
+		Fault:       8,
+		Lease:       8,
+		UsedBy:      "",
+		State:       8,
 		CreatedTime: time.Now().AddDate(2, 1, 3),
 	}
 	ctx := context.Background()
 
 	assert.NotNil(s.Validate(ctx))
-	s.Fault = "critical"
+	s.Fault = zebra.Critical
 
 	assert.NotNil(s.Validate(ctx))
-	s.Lease = "free"
+	s.Lease = zebra.Free
 
 	assert.NotNil(s.Validate(ctx))
-	s.State = "inactive"
+	s.State = zebra.Inactive
 
 	assert.NotNil(s.Validate(ctx))
 	s.CreatedTime = time.Now()
 
 	assert.Nil(s.Validate(ctx))
+}
+
+func TestString(t *testing.T) {
+	t.Parallel()
+	assert := assert.New(t)
+
+	var f zebra.Fault = 8
+
+	var l zebra.Lease = 8
+
+	var s zebra.State = 8
+
+	assert.Equal("unknown", f.String())
+	assert.Equal("unknown", l.String())
+	assert.Equal("unknown", s.String())
+}
+
+func TestUnmarshal(t *testing.T) {
+	t.Parallel()
+	assert := assert.New(t)
+
+	body := `8`
+
+	var f *zebra.Fault
+
+	var l *zebra.Lease
+
+	var s *zebra.State
+
+	assert.NotNil(f.UnmarshalText([]byte(body)))
+	assert.NotNil(l.UnmarshalText([]byte(body)))
+	assert.NotNil(s.UnmarshalText([]byte(body)))
 }
