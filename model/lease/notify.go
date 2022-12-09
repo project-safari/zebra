18fd74d41ff4d504f955f5a7f1e10f4f3860570b
Eva Achim <eva.achim@bison.howard.edu>
2022-12-09 15:02:44 -0500

    Enhance
    Signed-off-by: Eva Achim <eva.achim@bison.howard.edu>

--- model/lease/notify.go
+++ model/lease/notify.go
@@ -1,45 +1,127 @@
 package lease
 
 import (
+	"crypto/tls"
 	"fmt"
 	"log"
+	"net/mail"
 	"net/smtp"
 	"os"
+	"sync"
 )
 
-var SMTP_Host = os.Getenv("HOST")
+var (
+	From_mail     = "admin@zebra.project-safari.io"
+	Mail_password = "Riddikulus"
+	SMTP_Host     = os.Getenv("HOST")
+	Mail_subject  string
+	Mail_body     string
+
+	from      *mail.Address
+	auth      smtp.Auth
+	tlsconfig *tls.Config
+	mailwg    sync.WaitGroup
+)
+
+type Container struct {
+	m       sync.Mutex
+	Headers map[string]string
+}
+
+func NewContainer() *Container {
+	return &Container{
+		Headers: make(map[string]string),
+	}
+}
 
 // function to notify user once lease is satisfied.
 func (r *ResourceReq) Notify() {
 	satisfactionStatus := r.IsSatisfied()
 
+	strOne := "This is a notification to let you know that your lease request for resource "
+	strTwo := " is satisfied.\nLog back in to check it out!"
+	message := strOne + r.Name + " " + r.Type + strTwo
+	user := "user@zebra.project-safari.io"
+
 	if satisfactionStatus == true {
-		r.SendNotification()
+		r.SendNotification("Zebra Lease Request Satisfied", message, user)
 	}
 }
 
 // function to email notification.
-func (r *ResourceReq) SendNotification() {
-	// probably need a custom email address for the zebra server.
-	from_mail := "admin@zebra.project-safari.io"
-	from_pass := "Riddikulus"
+func (r *ResourceReq) SendNotification(subject, msg string, recipient string) {
+	to := mail.Address{Name: "", Address: recipient}
 
-	// need a function to get specific's user's email address.
-	recipient := []string{}
-	recipient = append(recipient, "user@zebra.project-safari.io")
+	Mail_subject = subject
+	Mail_body = msg
 
-	// Message.
-	message := []byte("Your lease request for resource " + r.Name + " of type " + r.Type + "has been satisfied. \nYou can now check it out on Zebra.") //nolint:lll
+	// initialize new container object
+	container := NewContainer()
+	// call mutex.lock to avoid multiple writes to
+	// one header instance from running goroutines
+	container.m.Lock()
+	container.Headers["From"] = from.String()
+	container.Headers["To"] = to.String()
+	container.Headers["Subject"] = Mail_subject
+	// unlock mutex after function returns
+	defer container.m.Unlock()
 
-	// Authentication.
-	auth := smtp.PlainAuth("", from_mail, from_pass, SMTP_Host)
+	// Setup message
+	message := ""
+	for k, v := range container.Headers {
+		message += fmt.Sprintf("%s: %s\r\n", k, v)
+	}
+	message += "\r\n" + Mail_body
 
-	fmt.Println(auth)
-	// Sending email.
-	if err := smtp.SendMail(fmt.Sprintf("%s:%d", SMTP_Host, 587), auth, from_mail, recipient, message); err != nil {
+	conn, err := tls.Dial("tcp", fmt.Sprintf("%s:%d", SMTP_Host, 465), tlsconfig)
+	if err != nil {
 		log.Printf("Error sending mail %v", err)
 		return
 	}
 
-	log.Println("Email Sent Successfully!")
+	c, err := smtp.NewClient(conn, SMTP_Host)
+	if err != nil {
+		log.Printf("Error sending mail %v", err)
+		return
+	}
+
+	// Auth
+	if err = c.Auth(auth); err != nil {
+		log.Printf("Error sending mail %v", err)
+		return
+	}
+
+	// To && From
+	if err = c.Mail(from.Address); err != nil {
+		log.Printf("Error sending mail %v", err)
+		return
+	}
+
+	if err = c.Rcpt(to.Address); err != nil {
+		log.Printf("Error sending mail %v", err)
+		return
+	}
+
+	// Data
+	w, err := c.Data()
+	if err != nil {
+		log.Printf("Error sending mail %v", err)
+		return
+	}
+
+	_, err = w.Write([]byte(message))
+	if err != nil {
+		log.Printf("Error sending mail %v", err)
+		return
+	}
+
+	err = w.Close()
+	if err != nil {
+		log.Printf("Error sending mail %v", err)
+		return
+	}
+
+	if err = c.Quit(); err != nil {
+		return
+	}
 }
