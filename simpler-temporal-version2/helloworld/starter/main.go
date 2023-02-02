bfa904000e7e9d864b974a1c8cee3db8d99163cc
Eva Achim <eva.achim@bison.howard.edu>
2023-02-02 13:28:27 -0500

    Small change
    Signed-off-by: Eva Achim <eva.achim@bison.howard.edu>

--- simpler-temporal-version2/helloworld/starter/main.go
+++ simpler-temporal-version2/helloworld/starter/main.go
@@ -1,23 +1,72 @@
-/* OLD WAY*/
-
 package main
 
 import (
 	"context"
 	"fmt"
+
 	"log"
+	"sync"
 
 	"go.temporal.io/sdk/client"
+	"go.temporal.io/sdk/worker"
 
 	"github.com/temporalio/samples-go/helloworld"
 )
 
-func printResults(greeting string, workflowID, runID string) {
-	fmt.Printf("\nWorkflowID: %s RunID: %s\n", workflowID, runID)
-	fmt.Printf("\n%s\n\n", greeting)
+func main() {
+	var count = 2
+	var wg sync.WaitGroup
+
+	wg.Add(count)
+
+	go firstClient(&wg)
+
+	defer wg.Done()
+
+	go secondClient(&wg)
+
+	wg.Wait()
 }
 
-func main() {
+func getName() string {
+	fmt.Println("Enter Your First Name: ")
+
+	// first name
+	var first string
+
+	// Taking input from user.
+	fmt.Scanln(&first)
+	fmt.Println("Enter Your Last Name: ")
+
+	// second name
+	var second string
+
+	// Taking input from the user.
+	fmt.Scanln(&second)
+
+	return first + " " + second
+}
+
+func firstClient(wg *sync.WaitGroup) {
+	// The client and worker are heavyweight objects that should be created once per process.
+	c, err := client.Dial(client.Options{})
+	if err != nil {
+		log.Fatalln("Unable to create client", err)
+	}
+	defer c.Close()
+
+	w := worker.New(c, "hello-world", worker.Options{})
+
+	w.RegisterWorkflow(helloworld.Workflow)
+	w.RegisterActivity(helloworld.Activity)
+
+	err = w.Run(worker.InterruptCh())
+	if err != nil {
+		log.Fatalln("Unable to start worker", err)
+	}
+}
+
+func secondClient(wg *sync.WaitGroup) {
 	// The client is a heavyweight object that should be created once per process.
 	c, err := client.Dial(client.Options{})
 	if err != nil {
@@ -38,131 +87,7 @@ func main() {
 	log.Println("Started workflow", "WorkflowID", we.GetID(), "RunID", we.GetRunID())
 
 	// Synchronously wait for the workflow completion.
-	var result string
-	err = we.Get(context.Background(), &result)
-	if err != nil {
-		log.Fatalln("Unable get workflow result", err)
-	}
-	// log.Println("Workflow result:", result)
-	printResults(result, we.GetID(), we.GetRunID())
-
-}
-
-/*
-func getName() string {
-	fmt.Println("Enter Your First Name: ")
-
-	// first name
-	var first string
-
-	// Taking input from user.
-	fmt.Scanln(&first)
-	fmt.Println("Enter Second Last Name: ")
-
-	// second name
-	var second string
-
-	// Taking input from the user.
-	fmt.Scanln(&second)
-
-	return first + " " + second
-}
-*/
-/*New way*/
-/*
-package main
-
-import (
-	"context"
-	"fmt"
-
-	"sync"
-	"log"
-
-	"go.temporal.io/sdk/client"
-	"go.temporal.io/sdk/worker"
-
-	"github.com/temporalio/samples-go/helloworld"
-)
-
-func main() {
-	var count = 2
-	var wg sync.WaitGroup
-
-	wg.Add(count)
-
-
-go firstClient(&wg)
-
-   defer wg.Done()
-
-   go secondClient(&wg)
-
-wg.Wait()
-}
-
-func getName() string {
-	fmt.Println("Enter Your First Name: ")
-
-	// first name
-	var first string
-
-	// Taking input from user.
-	fmt.Scanln(&first)
-	fmt.Println("Enter Second Last Name: ")
-
-	// second name
-	var second string
-
-	// Taking input from the user.
-	fmt.Scanln(&second)
-
-	return first + " " + second
-}
-
-
-func secondClient(wg *sync.WaitGroup){
-		// The client and worker are heavyweight objects that should be created once per process.
-	c, err := client.Dial(client.Options{})
-	if err != nil {
-		log.Fatalln("Unable to create client", err)
-	}
-	defer c.Close()
-
-	w := worker.New(c, "hello-world", worker.Options{})
-
-	w.RegisterWorkflow(helloworld.Workflow)
-	w.RegisterActivity(helloworld.Activity)
-
-	err = w.Run(worker.InterruptCh())
-	if err != nil {
-		log.Fatalln("Unable to start worker", err)
-	}
-	}
-
-
-func firstClient(wg *sync.WaitGroup) {
-// The client is a heavyweight object that should be created once per process.
-	c, err := client.Dial(client.Options{})
-	if err != nil {
-		log.Fatalln("Unable to create client", err)
-	}
-	defer c.Close()
-
-	workflowOptions := client.StartWorkflowOptions{
-		ID:        "hello_world_workflowID",
-		TaskQueue: "hello-world",
-	}
-
-	we, err := c.ExecuteWorkflow(context.Background(), workflowOptions, helloworld.Workflow, "Temporal")
-	if err != nil {
-		log.Fatalln("Unable to execute workflow", err)
-	}
-
-	log.Println("Started workflow", "WorkflowID", we.GetID(), "RunID", we.GetRunID())
-
-	// Synchronously wait for the workflow completion.
-	var result string
+	var result = getName()
 	err = we.Get(context.Background(), &result)
 	if err != nil {
 		log.Fatalln("Unable get workflow result", err)
@@ -170,4 +95,3 @@ func firstClient(wg *sync.WaitGroup) {
 	log.Println("Workflow result:", result)
 
 }
-*/
