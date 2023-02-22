ca1070336a66dc6d4cdbdd75a2720682b9287834
Eva Achim <eva.achim@bison.howard.edu>
2023-02-22 14:34:37 -0500

    Added var for workflow
    Signed-off-by: Eva Achim <eva.achim@bison.howard.edu>

--- model/noifications/authenticationNote.go
+++ model/noifications/authenticationNote.go
@@ -1,5 +1,9 @@
 package noifications
 
-func NotifyLogin() string {
-	return "This is a notification to let you know that there has been a new login into your account."
+var Notified bool //nolint:gochecknoglobals
+
+func NotifyLogin() (string, bool) {
+	Notified = true
+
+	return "This is a notification to let you know that there has been a new login into your account.", Notified
 }
