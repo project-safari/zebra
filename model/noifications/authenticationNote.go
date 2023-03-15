50c8e7bf9489c8beab98797b97505ff6c92f689d
Eva Achim <eva.achim@bison.howard.edu>
2023-03-15 10:40:05 -0400

    Fixed CodeClimate
    Signed-off-by: Eva Achim <eva.achim@bison.howard.edu>

--- model/noifications/authenticationNote.go
+++ model/noifications/authenticationNote.go
@@ -1,8 +1,8 @@
 package noifications
 
-var noteType string //nolint:gochecknoglobals
+var NoteType string //nolint:gochecknoglobals
 
 func NotifyLogin() (string, string) {
-	noteType = "l"
-	return "This is a notification to let you know that there has been a new login into your account.", noteType
+	NoteType = "l"
+	return "This is a notification to let you know that there has been a new login into your account.", NoteType
 }
--- model/noifications/registerNote.go
+++ model/noifications/registerNote.go
@@ -1,8 +1,6 @@
 package noifications
 
-var noteType string //nolint:gochecknoglobals
-
 func NotifyRegister() (string, string) {
-	noteType = "r"
-	return "This is a notification to let you know that a new account was created for you with this email address.", noteType
+	NoteType = "r"
+	return "This is a notification to let you know that a new account was created for you with this email address.", NoteType
 }
