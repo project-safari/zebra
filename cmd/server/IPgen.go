d6aa551e0f4a833bfa8664ae526ea6561e17f88d
Eva Achim <eva.achim@bison.howard.edu>
2023-07-10 15:31:20 -0400

    Appended model to preliminary IP alloc
    Signed-off-by: Eva Achim <eva.achim@bison.howard.edu>

--- cmd/server/IPgen.go
+++ cmd/server/IPgen.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-	"encoding/binary"
+	"fmt"
 	"math/rand"
-	"net"
+	"strconv"
+	"strings"
 )
 
-var ipInUse []net.IP
+var ipInUse []int
 
-func contains(all []net.IP, one net.IP) bool {
+func contains(all []int, one int) bool {
 	for _, each := range all {
-		eachStr := string(each)
-		str := string(one)
+		eachStr := fmt.Sprint(each)
+		str := fmt.Sprint(one)
 		if eachStr == str {
 			return true
 		}
@@ -20,51 +21,111 @@ func contains(all []net.IP, one net.IP) bool {
 	return false
 }
 
-func generateIP() (net.IP, []net.IP, error) {
-	buf := make([]byte, 4)
+func genNumericValue() int {
+	// numeric possibilities for the decimal value of the IP.
+	possibilities := "0123456789"
 
-	for i := 0; i < 10; i++ {
+	// the length of decimal, numeric value IP can be anywhere between 6 and 12.
+	min := 6
+	max := 12
 
-		ip := rand.Uint32()
-
-		binary.LittleEndian.PutUint32(buf, ip)
-	}
-
-	cidr := string(buf[:])
+	// generate the size randomly for each IP address.
+	size := rand.Intn(max-min) + min
 
 GENERATE:
-
-	ip, ipnet, err := net.ParseCIDR(cidr)
-	if err != nil {
-		return nil, nil, err
+	// create random IP values from the possible numbers in the possibilities string.
+	b := make([]byte, size)
+	for i := range b {
+		b[i] = possibilities[rand.Intn(len(possibilities))]
 	}
 
-	ones, _ := ipnet.Mask.Size()
-	quotient := ones / 8
-	remainder := ones % 8
+	// get the IP together as a decimal.
+	decimalIP, _ := strconv.Atoi((string(b)))
 
-	r := make([]byte, 4)
-	rand.Read(r)
-
-	for i := 0; i <= quotient; i++ {
-		if i == quotient {
-			shifted := byte(r[i]) >> remainder
-			r[i] = ^ipnet.IP[i] & shifted
-		} else {
-			r[i] = ipnet.IP[i]
-		}
-	}
-	ip = net.IPv4(r[0], r[1], r[2], r[3])
-
-	if contains(ipInUse, ip) {
+	if contains(ipInUse, decimalIP) {
 		goto GENERATE
 	} else {
-		ipInUse = append(ipInUse, ip)
+		ipInUse = append(ipInUse, decimalIP)
 	}
 
-	if ip.Equal(ipnet.IP) {
-		goto GENERATE
+	return decimalIP
+}
+
+func convertToHex(integerIP int) string {
+	// hexIP := strconv.FormatInt(255, integerIP)
+	hexIP := fmt.Sprintf("0x%x", integerIP)
+	hexIP = strings.TrimLeft(hexIP, "0x")
+	return hexIP
+}
+
+func getFinalIP(hexIP string) []string {
+	// finalHex := ""
+
+	l := len(hexIP)
+	manyBytes := (l / 2)
+
+	if (l % 2) != 0 {
+		manyBytes = manyBytes + 1
 	}
 
-	return ip, ipInUse, nil
+	arrForIP := make([]string, manyBytes)
+
+	for i := 0; i <= len(hexIP); i++ {
+		if i+2 <= len(hexIP) {
+			piece := hexIP[i : i+2]
+			arrForIP = append(arrForIP, piece)
+		}
+	}
+
+	for i, j := 0, len(arrForIP)-1; i < j; i, j = i+1, j-1 {
+		arrForIP[i], arrForIP[j] = arrForIP[j], arrForIP[i]
+	}
+
+	// finalHex = strings.Join(arrForIP, "")
+
+	return arrForIP
+}
+
+func hexToIP(hexVal []string) string {
+	var theIP []string
+	start := 0
+	end := 2
+	var piece string
+	var finalIP string
+
+	for i := 0; i < (len(hexVal))/2; i++ {
+		if end <= len(hexVal)-2 && start <= len(hexVal)-4 {
+			theIP = hexVal[start:end]
+			piece = strings.Join(theIP, "")
+			decimal, err := strconv.ParseInt(piece, 16, 64)
+			//in case of any error.
+			if err != nil {
+				panic(err)
+			}
+			finalIP = finalIP + fmt.Sprint(decimal) + "."
+
+			fmt.Println(theIP)
+		} else {
+			theIP = hexVal[len(hexVal)-2 : len(hexVal)-0]
+			piece = strings.Join(theIP, "")
+			decimal, err := strconv.ParseInt(piece, 16, 64)
+			//in case of any error.
+			if err != nil {
+				panic(err)
+			}
+			finalIP = finalIP + fmt.Sprint(decimal)
+
+			fmt.Println(theIP)
+		}
+
+		start = start + 2
+		end = end + 2
+
+	}
+
+	return ""
+}
+
+func main() {
+	fmt.Println(getFinalIP(convertToHex(genNumericValue())))
 }
